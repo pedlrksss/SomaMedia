6c35890d37cb95bf97dd30b46cd3469a10e62011
pedro <p.martims120@gmail.com>
2023-10-11 14:28:51 -0300

    SomaMedia

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -6,6 +6,36 @@ namespace Project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("PROGRAMA PARA CALCULAR A MEDIA ENTRE 2 OU MAIS NUMEROS");
+            Console.WriteLine("------------------------------------------------------");
+            
+            Console.WriteLine();
+            Console.WriteLine("Quantos numeos deseja informar? ");
+            int n = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite o valor: ");
+               int v = int.Parse(Console.ReadLine());
+                list.Add(v);  
+            }
+            foreach (var item in list)
+            { 
+                sum += item;
+            }
+            if (n >= 2) 
+            {
+                int result = sum / n; 
+                Console.WriteLine("Soma: " + sum);
+                Console.WriteLine("Média: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Não é possível calcular a média.");
+            }
+
 
         }
     }
